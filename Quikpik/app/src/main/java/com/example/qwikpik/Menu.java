e903b777898240db0ef8c1b275e48ee3c4c1f367
Éric Larivière <eric.lariiviere1999@hotmail.com>
2020-04-20 10:22:52 -0400

    Couleur perso

--- Quikpik/app/src/main/java/com/example/qwikpik/Menu.java
+++ Quikpik/app/src/main/java/com/example/qwikpik/Menu.java
@@ -3,6 +3,8 @@ package com.example.qwikpik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView textView = findViewById(R.id.textView_title);
+        String text = "<font color=#6AEE6A>Q</font>" +
+                "<font color=#F1A247>W</font>" +
+                "<font color=#763C76>I</font>" +
+                "<font color=#3535B9>K</font>" +
+                "<font > </font>" +
+                "<font color=#BC2B2D>P</font>" +
+                "<font color=#763C76>I</font>" +
+                "<font color=#3535B9>K</font>";
+        textView.setText(Html.fromHtml(text));
     }
 }
