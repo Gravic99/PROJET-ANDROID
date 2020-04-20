c9f3c1616750fd0778a38bfaf700b2330059fab3
Pier-Luc Nadeau <1732895@cegepba.qc.ca>
2020-04-20 09:33:14 -0400

    Animation completed

--- Quikpik/app/src/main/java/com/example/qwikpik/MainActivity.java
+++ Quikpik/app/src/main/java/com/example/qwikpik/MainActivity.java
@@ -3,16 +3,40 @@ package com.example.qwikpik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,Menu.class);
-        startActivity(intent);
+        StartUpAnimation();
+
+    }
+
+    private void StartUpAnimation() {
+        imageView = findViewById(R.id.imageView);
+        if(imageView == null) throw new AssertionError();
+        imageView.setBackgroundResource(R.drawable.animation_startup);
+        anim = (AnimationDrawable)imageView.getBackground();
+        Handler handlerAnimSwitch = new Handler();
+        final Intent intentMenu = new Intent(this, Menu.class);
+        anim.start();
+        handlerAnimSwitch.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                anim.stop();
+                startActivity(intentMenu);
+            }
+        },4200);
     }
 }
