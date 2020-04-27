9e8291779dad68573ad55745eb5d60af97282d9d
Éric Larivière <eric.lariiviere1999@hotmail.com>
2020-04-27 11:54:50 -0400

    Take Picture

--- Quikpik/app/src/main/java/com/example/qwikpik/CameraActivity.java
+++ Quikpik/app/src/main/java/com/example/qwikpik/CameraActivity.java
@@ -1,14 +1,33 @@
 package com.example.qwikpik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
     Button backToMenuButton;
+    Button btn_TakePicture;
+    ImageView ImageView_Picture;
+    String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +35,40 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
 
         backToMenuButton = findViewById(R.id.btn_Back);
+        btn_TakePicture = findViewById(R.id.btn_TakePicture);
+        ImageView_Picture = findViewById(R.id.imageView_Picture);
+        requestPermission();
         setListener();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Toast.makeText(this,Integer.toString(requestCode), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,Integer.toString(resultCode), Toast.LENGTH_SHORT).show();
+
+        if(resultCode == RESULT_OK){
+
+            if(requestCode == 1){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                ImageView_Picture.setImageBitmap(bitmap);
+
+
+            }
+        }
+
+
+
+    }
+    private void requestPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+    }
     private void setListener() {
         backToMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,10 +76,45 @@ public class CameraActivity extends AppCompatActivity {
                 goToActivity(Menu.class);
             }
         });
+        btn_TakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchPictureTakeAction();
+            }
+        });
     }
 
     private void goToActivity(Class activityToGoTo){
         Intent activityIntent = new Intent(this,activityToGoTo);
         startActivity(activityIntent);
+    }
+    private void dispatchPictureTakeAction(){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePicture.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+            if(photoFile != null){
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(CameraActivity.this,"com.example.qwikpik",photoFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(takePicture,1);
+
+
+            }
+
+        }
+    }
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File StorageDir = Environment.getExternalStoragePublicDirectory((Environment.DIRECTORY_PICTURES));
+        File image = null;
+
+        try {
+            image = File.createTempFile(name,".jpg",StorageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return image;
+
     }
 }
