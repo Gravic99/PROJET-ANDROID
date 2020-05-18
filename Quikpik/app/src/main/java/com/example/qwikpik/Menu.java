f9dce3bac32b75f746f9615c050f69aa940c68f4
Pier-Luc Nadeau <1732895@cegepba.qc.ca>
2020-05-18 10:48:43 -0400

    REELEASE

--- Quikpik/app/src/main/java/com/example/qwikpik/Menu.java
+++ Quikpik/app/src/main/java/com/example/qwikpik/Menu.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Application;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -45,8 +46,6 @@ public class Menu extends AppCompatActivity {
                 ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA,
                     Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},1);
-            Toast.makeText(this,"Veuillez redémarrer l'application pour assurer son bon fonctionnement.",Toast.LENGTH_LONG).show();
-
         }
 
         mLocationRequest = new LocationRequest();
@@ -72,6 +71,13 @@ public class Menu extends AppCompatActivity {
         goToGalleryButton = findViewById(R.id.btn_Gallery);
         setListener();
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Toast.makeText(this,"Veuillez redémarrer l'application pour assurer son bon fonctionnement.",Toast.LENGTH_LONG).show();
+        System.exit(0);
     }
 
     @Override
